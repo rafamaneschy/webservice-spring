f0894addeb8ad39c478b804ccfb7a2f9eeb604a8
Rafael Maneschy <rafamaneschy@gmail.com>
2021-09-27 12:57:15 -0300

    Project created

---
+++ src/main/java/com/rafamaneschy/webservicespring/WebserviceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rafamaneschy.webservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserviceSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafamaneschy/webservicespring/WebserviceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafamaneschy.webservicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
